b4218f2e709a3ad9a6769275422cf4604632839a
Azzam Ubaidillah <azzam.ubaidillah05@gmail.com>
2023-08-10 10:32:59 +0700

    swiftylab

---
+++ SwiftyLabExample/SwiftyLabExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyLabExampleApp.swift
+//  SwiftyLabExample
+//
+//  Created by Azzam Ubaidillah on 10/08/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyLabExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
